c3b4ce815cbe768612a8d37aafdd20383392d4dc
Jomit <jovagh@microsoft.com>
2016-12-13 12:59:48 -0800

    inital device registration app

---
+++ CreateDeviceIdentity/CreateSelfSignedCert.ps1
@@ -0,0 +1,8 @@
+﻿$Password = "pass@word1"
+$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+
+$CertDNSName = "boeing.iothub.test"
+$CertFileFullPath = $pwd.Path + '\' + $CertDNSName + '.pfx'
+
+$NewCert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName $CertDNSName
+Export-PfxCertificate -FilePath $CertFileFullPath -Password $SecurePassword -Cert $NewCert
---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "";
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "aircraft-101";
+            Device device;
+            try
+            {
+                //device = await registryManager.AddDeviceAsync(new Device(deviceId));
+
+                device = await registryManager.AddDeviceAsync(new Device(deviceId)
+                {
+                    Authentication = new AuthenticationMechanism()
+                    {
+                        X509Thumbprint = new X509Thumbprint()
+                        {
+                            PrimaryThumbprint = "6b76c6d4ca1db2825b2abcd26b8988981b597539"
+                        }
+                    }
+                });
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            //catch (Exception ex)
+            //{
+            //    device = null;
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+            Console.WriteLine("Generated device key: {0}", device.Authentication.X509Thumbprint.PrimaryThumbprint);
+        }
+
+    }
+}
