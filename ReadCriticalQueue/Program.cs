f1761ce0efabb4a7709c300c423a057938bcf798
Jomit <jovagh@microsoft.com>
2016-12-13 14:38:03 -0800

    added critical queue reading app added method to route device messages using level="critical" property

---
+++ ReadCriticalQueue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReadCriticalQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive critical messages. Ctrl-C to exit.\n");
+            var connectionString = "Endpoint=sb://jomitservicebus.servicebus.windows.net/;SharedAccessKeyName=ListenerAccessKey;SharedAccessKey=XCR+SjXIEA9DZCYQEgGdsEica1p6fpjHBUDYPi4LKNs=";
+            var queueName = "ac-critial-msg";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                //try
+                //{
+                Stream stream = message.GetBody<Stream>();
+                StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+                string s = reader.ReadToEnd();
+                Console.WriteLine(String.Format("Message Id : {0} => Message body: {1}", message.MessageId, s));
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex.Message);
+                //}
+            });
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- SimulatedDevice/Program.cs
+++ SimulatedDevice/Program.cs
@@ -22,7 +22,8 @@ namespace SimulatedDevice
             //deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceThumbprint));
             deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithX509Certificate(deviceId, new X509Certificate2("boeing.iothub.test.pfx", "pass@word1")));
 
-            SendDeviceToCloudMessagesAsync(deviceId);
+            //SendDeviceToCloudMessagesAsync(deviceId);
+            SendDeviceToCloudWithCriticalMessagesAsync(deviceId);
             Console.ReadLine();
         }
 
@@ -49,6 +50,44 @@ namespace SimulatedDevice
                 Task.Delay(1000).Wait();
             }
         }
+
+        private static async void SendDeviceToCloudWithCriticalMessagesAsync(string deviceId)
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = deviceId,
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                string levelValue;
+
+                if (rand.NextDouble() > 0.7)
+                {
+                    messageString = "This is a critical message";
+                    levelValue = "critical";
+                }
+                else
+                {
+                    levelValue = "normal";
+                }
+
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("level", levelValue);  //just a random property to identify critical messages
+
+                await deviceClient.SendEventAsync(message); // Need to add transient fault handling in production : https://msdn.microsoft.com/library/hh680901(v=pandp.50).aspx
+                Console.WriteLine("{0} > Sent message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+        }
+
 
     }
 }
